e33d4e371b784e4fde0204a67f8396e8f12a300a
insomnial <akurt6805@protonmail.com>
2025-07-04 16:14:49 -0700

    Setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='atlassian-api-py',
+   name='atlassian_api_py',
    version='0.1',
    description='Wrapper for Jira API',
    author='Kurt Anderson',
    author_email='akurt6805@protonmail.com',
-   packages=['atlassian-api-py'],  #same as name
+   packages=['app'],  #same as name
    install_requires=['certifi', 'charset-normalizer', 'idna', 'requests', 'urllib3'], #external packages as dependencies
 )
